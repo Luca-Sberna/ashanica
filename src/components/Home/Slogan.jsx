149ad185bbfb1f9c42066c8666755a495a50a9c1
luca sberna <luca.sberna@thinkopen.it>
2025-06-30 16:24:52 +0200

    adj

--- src/components/Home/Slogan.jsx
+++ src/components/Home/Slogan.jsx
@@ -11,35 +11,17 @@ const Slogan = () => {
       className={`${styles.sloganContainer} py-5 position-relative overflow-hidden`}
     >
       <Row className="justify-content-center align-items-cente position-relative">
-        {/* Virgoletta sinistra */}
-        <Col className="text-end pe-0">
-          <img
-            src={leftQuote}
-            alt="Virgoletta sinistra"
-            className={`${styles.quoteImageSx} img-fluid `}
-          />
-        </Col>
-
         {/* Testo centrale */}
         <Col xs={10} md={10} className="text-center px-0">
           <h2 className={`${styles.sloganText} mb-0`}>
             <span className={styles.firstLine}>
-              ASKNICA FEMMINILE PER NATURA...
+              ASKNICA FEMMINILE PER NATURA
             </span>
             <br />
-            <span className={styles.secondLine}>AUDACE PER SCELTA</span>
+            <span className={styles.secondLine}>...AUDACE PER SCELTA...</span>
             <br />
             <span className={styles.thirdLine}>L'ELEGANZA CON CARATTERE</span>
           </h2>
-        </Col>
-
-        {/* Virgoletta destra */}
-        <Col className="text-start ps-0">
-          <img
-            src={rightQuote}
-            alt="Virgoletta destra"
-            className={`${styles.quoteImageDx} img-fluid`}
-          />
         </Col>
       </Row>
     </Container>
